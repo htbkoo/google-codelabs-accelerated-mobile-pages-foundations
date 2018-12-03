ff9b76afcc7ceb5e6ed7daa7d005d11dae706723
htbkoo <h1017760@connect.hku.hk>
2018-12-03 21:52:41 +0800

    created server.js for simple http server htbkoo/google-codelabs/accelerated-mobile-pages-foundations

---
+++ simple-http-server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.static('accelerated-mobile-pages-foundations'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
