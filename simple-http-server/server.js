945d9aa431cbd38033813965234870d202bf5f77
htbkoo <h1017760@connect.hku.hk>
2018-12-03 21:53:57 +0800

    cleaned up server.js htbkoo/google-codelabs/accelerated-mobile-pages-foundations

--- simple-http-server/server.js
+++ simple-http-server/server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
 
-app.use(express.static('accelerated-mobile-pages-foundations'))
+app.use(express.static('accelerated-mobile-pages-foundations'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
